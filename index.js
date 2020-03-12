f07c3433a62f8802e8d55460f08650bd37e4301b
rbargues <rbargues@gmail.com>
2020-03-12 11:35:48 -0400

    fixed typo, missing const

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const jwt = require("jsonwebtoken");
 const cookieParser = require('cookie-parser')
 const port = 3000;
 const secret = process.env.SECRET;//fs.readFileSync(path.resolve(__dirname, "./secret.txt"),"UTF-8");
-dbURI = process.env.URI;
+const dbURI = process.env.URI;
 const pool = new Pool({
   connectionString: dbURI
 });
